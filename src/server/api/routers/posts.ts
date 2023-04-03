232647d67f15bd29f28387d8d6b1cf36b48edd21
CoppoloCode <matthewcoppolo@yahoo.com>
2023-04-03 16:05:59 -0400

    created profile page with styling

--- src/server/api/routers/posts.ts
+++ src/server/api/routers/posts.ts
@@ -5,16 +5,10 @@ import type {User} from "@clerk/nextjs/dist/api";
 import { createTRPCRouter, privateProcedure, publicProcedure } from "~/server/api/trpc";
 import { z } from "zod";
 
-const filterUserForClient = (user: User) => {
-  return {
-    id: user.id, 
-    username: user.username, 
-    profileImageUrl: user.profileImageUrl
-  };
-}
 
 import { Ratelimit } from "@upstash/ratelimit";
 import { Redis} from "@upstash/redis";
+import { filterUserForClient } from "~/server/helpers/filterUserForClient";
 
 const ratelimit = new Ratelimit({
   redis: Redis.fromEnv(),
