f7bf4113ed9d27b0b0e89035d663d582ae13f974
CoppoloCode <matthewcoppolo@yahoo.com>
2023-04-03 18:50:24 -0400

    fixed numbers being posted

--- src/server/api/routers/posts.ts
+++ src/server/api/routers/posts.ts
@@ -77,7 +77,7 @@ export const postsRouter = createTRPCRouter({
 ),
 
 create: privateProcedure.input(z.object({
-  content: z.string().emoji("Only emojis are allowed.").min(1).max(280), //guarantees data is an emoji between 1 280 char
+  content: z.string().emoji("Only emojis are allowed.").min(1).max(280).refine((v) => !/\d/.test(v)), //guarantees data is an emoji between 1 280 char
 })).mutation(async ({ctx, input}) => {
 
   const authorId = ctx.userId;
